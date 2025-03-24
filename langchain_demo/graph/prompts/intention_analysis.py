0d2209f9282856b0d84deec80a00f303e045cb54
kiragirl <liyiming0215@sina.com>
2025-03-24 10:16:38 +0800

    Something I cant remember.

--- langchain_demo/graph/prompts/intention_analysis.py
+++ langchain_demo/graph/prompts/intention_analysis.py
@@ -4,8 +4,6 @@ flight-tickets-search：用于搜索航班
 flight-dynamic：用于查询航班动态信息
 flight-booking-reservation：用于预订特定航班
 manage-my-booking：用于管理已预订的航班信息
-policy-enquiring：用于询问政策或规则，或者询问机票政策，或者询问机场信息，或者询问航司相关信息或者询问有关行李限额
-payment-assistant: 用于用户支付完成后确认支付结果和支付错误反馈
 self-service: 用户用户自助服务, 包括开具电子行程单, 开具航延证明, 不正常航班自动改期.
 请根据以下规则选择合适的角色：
 
@@ -13,8 +11,6 @@ self-service: 用户用户自助服务, 包括开具电子行程单, 开具航�
 如果用户想要查询航班动态信息，下一步应该是flight-dynamic。
 如果用户想要知道他已经预订的航班信息，下一步应该是manage-my-booking。
 如果用户想要预订特定航班，下一步应该是flight-booking-reservation。
-如果用户询问政策或规则，或者询问机票政策，或者询问机场信息，或者询问航司相关信息或者询问有关行李限额，下一步应该是policy-enquiring。
-如果用户回复支付成功或者支付遇到困难, 下一步应该是payment-assistant。
 如果用户想要进行自助服务，自助服务包括（开具航延证明、开具电子行程单、航班自助改期），下一步应该是self-service。
 如果你不确定或都不是，请选择flight-tickets-search。
 
@@ -34,15 +30,6 @@ AI：policy-enquiring
 
 用户：你能帮我查一下航班吗？
 AI：flight-tickets-search
-
-用户: 你好
-AI：flight-tickets-search
-
-用户: 我已经支付成功
-AI：payment-assistant
-
-用户: 你好，我想查询政策。
-AI：policy-enquiring
 
 用户：我要查询明天HU1234航班的信息
 AI： flight-dynamic
