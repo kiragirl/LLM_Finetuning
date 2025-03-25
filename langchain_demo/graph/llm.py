988ff3da60f3866d364af4f100d2c94180595a13
kiragirl <liyiming0215@sina.com>
2025-03-25 13:30:36 +0800

    Update llm.py

--- langchain_demo/graph/llm.py
+++ langchain_demo/graph/llm.py
@@ -16,12 +16,5 @@ tongyi_model = ChatOpenAI(
     streaming=False,
 )
 
-glm_model = ChatOpenAI(
-    openai_api_base="http://10.223.224.231:8088/api/paas/v4/",
-    openai_api_key="0e955cb8dace43add2fec28f08b74760.F5TIIcZYse62Kbcq",
-    model="glm-4",
-    temperature=0.1,
-    streaming=False,
-)
 
 
