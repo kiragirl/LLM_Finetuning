45768dd7266eb5b72ddf626271daaaf35af30616
kiragirl <liyiming0215@sina.com>
2025-03-25 13:25:11 +0800

    Something I cant remember.

--- langchain_demo/graph/llm.py
+++ langchain_demo/graph/llm.py
@@ -16,12 +16,10 @@ tongyi_model = ChatOpenAI(
     streaming=False,
 )
 
-glm_model = ChatOpenAI(
-    openai_api_base="http://10.223.224.231:8088/api/paas/v4/",
-    openai_api_key="0e955cb8dace43add2fec28f08b74760.F5TIIcZYse62Kbcq",
-    model="glm-4",
+moonshot_model = ChatOpenAI(
+    openai_api_base="https://api.moonshot.cn/v1/",
+    openai_api_key=os.environ["MOON_SHOT_API_KEY"],
+    model="moonshot-v1-32k",
     temperature=0.1,
     streaming=False,
 )
-
-
